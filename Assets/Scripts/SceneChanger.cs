33b8d3ce17117c53a4934218620107c385c0a4b7
JAVI\Javier <javi.valda@gmail.com>
2019-03-20 19:14:33 -0300

    SceneManager hecho en clase

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour
+{
+    string sceneToLoad;
+    public static SceneChanger instance = null;
+    public Slider UISlider;
+
+
+    private void Start()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            sceneToLoad = "Scene1";
+            LoadScene(sceneToLoad);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            sceneToLoad = "Scene2";
+            LoadScene(sceneToLoad);
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            sceneToLoad = "Scene3";
+            LoadScene(sceneToLoad);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine("AScene");
+    }
+
+    IEnumerator AScene()
+    {
+        AsyncOperation loadingProgress = SceneManager.LoadSceneAsync("Scenes/Scene2");
+
+        while (!loadingProgress.isDone)
+        {
+            UISlider.value = loadingProgress.progress;
+            print("Progress: " + loadingProgress.progress);
+            yield return null;
+        }
+    }
+}
