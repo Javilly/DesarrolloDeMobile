3974c93795a69d178172168427484434369de76a
Javilly <jajayoda@hotmail.com>
2019-05-30 18:40:22 -0300

    Camara y sacar palitos terminado, tablero terminado y bolitas puestas

--- Assets/Game2/Scripts/Giroscopio.cs
+++ Assets/Game2/Scripts/Giroscopio.cs
@@ -9,6 +9,13 @@ public class Giroscopio : MonoBehaviour
     public float speedMod;
     //private Rigidbody rb;
 
+    Vector2 startPos;
+    float minSwipeDist = -100f;
+
+    Ray ray;
+    RaycastHit hit;
+    GameObject touchedObject;
+
     private void Start()
     {
         Input.gyro.enabled = true;
@@ -16,11 +23,17 @@ public class Giroscopio : MonoBehaviour
         //rb = GetComponent<Rigidbody>();
     }
 
+    private void MoveAndDestroy(GameObject objectToMove)
+    {
+        objectToMove.transform.Translate(-touchedObject.transform.right * 8);
+        Destroy(objectToMove, 2f);
+    }
+
     private void Update()
     {
         transform.LookAt(cube.transform);
-        print(Input.gyro.rotationRate);
-
+        //print(Input.gyro.rotationRate);
+        //print(Input.touchCount);
 
         if (Input.touchCount == 0)
         {
@@ -31,6 +44,48 @@ public class Giroscopio : MonoBehaviour
             if (gyro.rotationRate.y < -0.05)
             {
                 transform.RotateAround(cube.transform.position, Vector3.up, Time.deltaTime * speedMod);
+            }
+        }
+        else if (Input.touchCount == 2)  //&& Input.GetTouch(1).phase == TouchPhase.Began
+        {
+            bool moveObject = false;
+
+            switch (Input.GetTouch(1).phase)
+            {
+                case TouchPhase.Began:
+                    startPos = Input.GetTouch(1).position;
+
+                    ray = Camera.main.ScreenPointToRay(Input.GetTouch(1).position);
+                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 100f);
+                    break;
+
+
+                case TouchPhase.Ended:
+                    float swipeDist = Input.GetTouch(1).position.y - startPos.y;
+
+                    print(swipeDist);
+
+                    if (swipeDist < minSwipeDist)
+                    {
+                        // It's a swiiiiiiiiiiiipe!
+                        //var swipeDirection = Mathf.Sign(Input.GetTouch(1).position.y - startPos.y);
+                        moveObject = true;
+                        print("ME MUEVOOOOOOO");
+                        // Do something here in reaction to the swipe.
+                    }
+                    break;
+            }
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.collider != null && moveObject)
+                {
+                    touchedObject = hit.transform.gameObject;
+                    Debug.Log("Touched " + touchedObject.transform.name);
+
+                    MoveAndDestroy(touchedObject);
+                }
             }
         }
     }
